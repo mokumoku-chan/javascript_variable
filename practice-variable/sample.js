5ec4d83693d78df44f29c2318e69a561e6a646cd
mokumoku-chan <maho.kumo1997@gmail.com>
2022-03-31 00:15:56 +0900

    Add variable

--- practice-variable/sample.js
+++ practice-variable/sample.js
@@ -1,5 +1,17 @@
 var nickname = "taro"
 console.log(nickname)
 
-var nickname = "ichiro"
+nickname = "ichiro"
 console.log(nickname)
+
+let nickname_1 = "taro"
+console.log(nickname_1)
+
+nickname_1 = "ichiro"
+console.log(nickname_1)
+
+const nickname_2 = "taro"
+console.log(nickname_2)
+
+nickname_2 = "ichiro"
+console.log(nickname_2)
