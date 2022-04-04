bae3981bbb6f1e7af4128e426fa691ea4d397d53
mokumoku-chan <maho.kumo1997@gmail.com>
2022-04-04 23:48:09 +0900

    Add function

--- practice-variable/sample.js
+++ practice-variable/sample.js
@@ -1,3 +1,21 @@
+
+let alertString;
+alertString = addString("WebCamp");
+
+alert(alertString)
+
+function addString(strA){
+  let addStr = "Hello " + strA;
+  return addStr;
+}
+
+let promptStr = prompt('何か好きな文字を入力してください。');
+alert(promptStr)
+
+
+
+
+
 var nickname = "taro"
 console.log(nickname)
 
